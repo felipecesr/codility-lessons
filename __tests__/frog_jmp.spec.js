5a5c26a845044dfa9a5d60c989634462e13f3523
Felipe César <felipecesr@gmail.com>
2020-10-27 08:10:17 -0300

    fix: frog jmp file name

--- __tests__/frog_jmp.spec.js
+++ __tests__/frog_jmp.spec.js
@@ -1,4 +1,4 @@
-const solution = require("../src/frog_imp");
+const solution = require("../src/frog_jmp");
 
 test("should return the minimal number of jumps", () => {
   expect(solution(10, 85, 30)).toBe(3);
